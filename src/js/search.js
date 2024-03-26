f0531d059a5c2cbb47348b0b767dabede70dbff1
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-03-26 17:12:43 +0200

    working on search results

---
+++ src/js/search.js
@@ -0,0 +1,36 @@
+const searchFrom = document.querySelector("#form");
+const searchInput = document.querySelector("#search");
+const resultList = document.querySelector(".searchResults");
+
+searchFrom.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchRecipes();
+});
+
+async function searchRecipes() {
+  const searchValue = searchInput.value.trim();
+  const response = await fetch(
+    `https://api.edamam.com/search?q=${searchValue}&app_id=34cb3eeb&app_key=48e9cfebbd16e50346d1adfcea396f8a&from=0&to=10`,
+  );
+  console.log(response);
+  const data = await response.json();
+  displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+  let html = "";
+  recipes.forEach((recipe) => {
+    html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `;
+  });
+
+  resultList.innerHTML = html;
+}
